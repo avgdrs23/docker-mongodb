ca3ec7dd23db0354544b799faba2953bc78fbe2b
avishay <avgdrs23@gmail.com>
2022-02-06 19:14:10 +0200

    stop file added

--- Run.sh
+++ Run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
-docker run -d  -p 27017:27017 -v mongo_data_directory:/data/db  -it $1:latest 
+image=$(basename $PWD)
+docker run -d  -p 27017:27017 -v mongo_data_directory:/data/db --name=$image  -it  $1:latest 
 #Run your Docker Image
 
